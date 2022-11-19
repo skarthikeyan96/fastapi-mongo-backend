0f3c9c148016a5737751ac7b7c080f59b542d09f
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2022-11-19 08:15:47 +0530

    feat: db connection and basic route

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from dotenv import dotenv_values
+from pymongo import MongoClient
+
+config = dotenv_values(".env")
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "API using Fast API and pymongo"}
+
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["MONGODB_CONNECTION_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
